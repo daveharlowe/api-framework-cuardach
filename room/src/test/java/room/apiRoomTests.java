97630146b7703dd8a5fed37678cdd80998df5a77
dave <daveharlowe@hotmail.com>
2021-07-04 02:01:46 +0100

    added 1 test case [duplicate of -> createRoom_10002_ver2()]

--- room/src/test/java/room/apiRoomTests.java
+++ room/src/test/java/room/apiRoomTests.java
@@ -131,6 +131,46 @@ public class apiRoomTests {
 	}
 	
 	
+	@Test
+	public void createRoom_10002_1_ver2()
+	{
+		OkHttpClient client = new OkHttpClient().newBuilder()
+				  .build();
+				MediaType mediaType = MediaType.parse("application/json");
+				RequestBody body = RequestBody.create(mediaType, "{\r\n  \"accessible\": false,\r\n  \"description\": \"Description of room, created using TAR_Okhttp3 client\",\r\n  \"features\": [],\r\n  \"image\": \"room2.jpg\",\r\n  \"roomNumber\": 111,\r\n  \"roomPrice\": 20,\r\n  \"roomid\": 111,\r\n  \"type\": \"Single\"\r\n}");
+				Request request = new Request.Builder()
+				  .url("http://vm102:3001/room/")
+				  .method("POST", body)
+				  .addHeader("Accept-Language", "en-US,en;q=0.9")
+				  .addHeader("Cookie", getCookieValue())
+				  .addHeader("Content-Type", "application/json")
+				  .build();
+				
+			    okhttp3.Response response=null;
+			    
+				try {
+					response = client.newCall(request).execute();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+//		//		int statusCode = response.getStatusCode();
+				int statusCode = response.code();
+				
+				try {
+					System.out.println("Test 10002_ver2 Response Body is =>  " + response.body().string());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				Assert.assertEquals(201,statusCode);
+	}
+	
+	
+	
+	
+	
 	/*
 	@Test
 	public void createRoom_10002()
